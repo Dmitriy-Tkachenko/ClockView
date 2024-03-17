92e47bea5ea25c47c150385b338e51bceb2c415a
Dmitriy Tkachenko <tk4dmitriy@gmail.com>
2024-03-18 00:14:46 +0300

    Added clock frame drawing with public color property. Added a custom attribute to change the frame color. Added default size for circle watch base.

--- customclockview/src/main/java/ru/tk4dmitriy/customclockview/ClockView.kt
+++ customclockview/src/main/java/ru/tk4dmitriy/customclockview/ClockView.kt
@@ -5,9 +5,15 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.core.content.withStyledAttributes
+import kotlin.math.min
+
+private const val STROKE_WIDTH_RATIO = 10
 
 class ClockView @JvmOverloads constructor(
     context: Context,
@@ -16,16 +22,23 @@ class ClockView @JvmOverloads constructor(
     private val defWidth: Int = resources.getDimension(R.dimen.defWidth).toInt()
     private val defHeight: Int = resources.getDimension(R.dimen.defHeight).toInt()
 
-    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        color = Color.RED
-    }
+    private var centerX = 0f
+    private var centerY = 0f
+    private var radius = 0f
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     var baseColor = 0
+    var frameColor = 0
 
     init {
         context.withStyledAttributes(attrs, R.styleable.ClockView, R.attr.clockViewStyle) {
             baseColor = getColor(
                 R.styleable.ClockView_baseColor,
+                ContextCompat.getColor(context, R.color.white)
+            )
+            frameColor = getColor(
+                R.styleable.ClockView_frameColor,
                 ContextCompat.getColor(context, R.color.black)
             )
         }
@@ -33,15 +46,30 @@ class ClockView @JvmOverloads constructor(
 
     private fun drawBase(canvas: Canvas) {
         paint.color = baseColor
-        canvas.drawCircle(width / 2f, height / 2f, width.coerceAtMost(height) / 2f, paint)
+        canvas.drawCircle(centerX, centerY, radius, paint)
+    }
+
+    private fun drawFrame(canvas: Canvas) {
+        paint.color = frameColor
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = radius / STROKE_WIDTH_RATIO
+        val adjustedRadius = radius - paint.strokeWidth / 2
+        canvas.drawCircle(centerX, centerY, adjustedRadius, paint)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        radius = width.coerceAtMost(height) / 2f
+        centerX = w / 2f
+        centerY = h / 2f
     }
 
     override fun onDraw(canvas: Canvas) {
         drawBase(canvas = canvas)
-        super.onDraw(canvas)
+        drawFrame(canvas = canvas)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         val width = resolveSize(defWidth, widthMeasureSpec)
         val height = resolveSize(defHeight, heightMeasureSpec)
 
