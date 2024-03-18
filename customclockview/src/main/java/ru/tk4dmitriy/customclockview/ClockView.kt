dd258376f468704ff882ea306d1b4d320934bc70
Dmitriy Tkachenko <tk4dmitriy@gmail.com>
2024-03-18 22:27:27 +0300

    Added drawing of the second hand based on the current second. Trigonometry is used to draw an second hand. Created separate paint for each clock element and moved the property settings from onDraw to init block.

--- customclockview/src/main/java/ru/tk4dmitriy/customclockview/ClockView.kt
+++ customclockview/src/main/java/ru/tk4dmitriy/customclockview/ClockView.kt
@@ -2,34 +2,44 @@ package ru.tk4dmitriy.customclockview
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.core.content.withStyledAttributes
-import kotlin.math.min
+import kotlin.math.cos
+import kotlin.math.sin
 
-private const val STROKE_WIDTH_RATIO = 10
+private const val FRAME_TO_RADIUS_RATIO = 2.0f / 21.0f
+private const val MAIN_LONG_PART_SECOND_HAND_TO_RADIUS_RATIO = 32.0f / 45.0f
+private const val MAIN_SHORT_PART_SECOND_HAND_TO_RADIUS_RATIO = 2.0f / 35.0f
+private const val ADDITIONAL_PART_SECOND_HAND_TO_RADIUS_RATIO = 9.0f / 35.0f
+private const val WIDTH_MAIN_PART_SECOND_HAND_TO_RADIUS_RATIO = 1.0f / 63.0f
+private const val WIDTH_ADDITIONAL_PART_SECOND_HAND_TO_RADIUS_RATIO = 1.0f / 35.0f
+private const val SECOND_TO_ANGLE_RATIO = Math.PI / 30
+private const val OFFSET_ANGLE = Math.PI / 2
 
 class ClockView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null
 ): View(context, attrs) {
+    private val basePaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val framePaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val mainPartSecondHandPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val additionalPartSecondHandPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+
     private val defWidth: Int = resources.getDimension(R.dimen.defWidth).toInt()
     private val defHeight: Int = resources.getDimension(R.dimen.defHeight).toInt()
 
     private var centerX = 0f
     private var centerY = 0f
     private var radius = 0f
-
-    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private var frameRadius = 0f
+    private var angle = 0f
 
     var baseColor = 0
     var frameColor = 0
+    var secondHandColor = 0
 
     init {
         context.withStyledAttributes(attrs, R.styleable.ClockView, R.attr.clockViewStyle) {
@@ -41,31 +51,69 @@ class ClockView @JvmOverloads constructor(
                 R.styleable.ClockView_frameColor,
                 ContextCompat.getColor(context, R.color.black)
             )
+            secondHandColor = getColor(
+                R.styleable.ClockView_frameColor,
+                ContextCompat.getColor(context, R.color.black)
+            )
         }
-    }
-
-    private fun drawBase(canvas: Canvas) {
-        paint.color = baseColor
-        canvas.drawCircle(centerX, centerY, radius, paint)
-    }
-
-    private fun drawFrame(canvas: Canvas) {
-        paint.color = frameColor
-        paint.style = Paint.Style.STROKE
-        paint.strokeWidth = radius / STROKE_WIDTH_RATIO
-        val adjustedRadius = radius - paint.strokeWidth / 2
-        canvas.drawCircle(centerX, centerY, adjustedRadius, paint)
+        basePaint.apply {
+            color = baseColor
+        }
+        framePaint.apply {
+            color = frameColor
+            style = Paint.Style.STROKE
+        }
+        mainPartSecondHandPaint.apply {
+            color = secondHandColor
+        }
+        additionalPartSecondHandPaint.apply {
+            color = secondHandColor
+        }
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         radius = width.coerceAtMost(height) / 2f
         centerX = w / 2f
         centerY = h / 2f
+
+        framePaint.strokeWidth = radius * FRAME_TO_RADIUS_RATIO
+        frameRadius = radius - framePaint.strokeWidth / 2
+
+        mainPartSecondHandPaint.strokeWidth =
+            radius * WIDTH_MAIN_PART_SECOND_HAND_TO_RADIUS_RATIO
+        additionalPartSecondHandPaint.strokeWidth =
+            radius * WIDTH_ADDITIONAL_PART_SECOND_HAND_TO_RADIUS_RATIO
+    }
+
+    private fun drawBase(canvas: Canvas) {
+        canvas.drawCircle(centerX, centerY, radius, basePaint)
+    }
+
+    private fun drawFrame(canvas: Canvas) {
+        canvas.drawCircle(centerX, centerY, frameRadius, framePaint)
+    }
+    private fun drawSecondHand(canvas: Canvas, second: Int) {
+        angle = (second * SECOND_TO_ANGLE_RATIO - OFFSET_ANGLE).toFloat()
+        canvas.drawLine(
+            centerX - cos(angle) * radius * MAIN_SHORT_PART_SECOND_HAND_TO_RADIUS_RATIO,
+            centerY - sin(angle) * radius * MAIN_SHORT_PART_SECOND_HAND_TO_RADIUS_RATIO,
+            centerX + cos(angle) * radius * MAIN_LONG_PART_SECOND_HAND_TO_RADIUS_RATIO,
+            centerY + sin(angle) * radius * MAIN_LONG_PART_SECOND_HAND_TO_RADIUS_RATIO,
+            mainPartSecondHandPaint
+        )
+        canvas.drawLine(
+            centerX - cos(angle) * radius * ADDITIONAL_PART_SECOND_HAND_TO_RADIUS_RATIO,
+            centerY - sin(angle) * radius * ADDITIONAL_PART_SECOND_HAND_TO_RADIUS_RATIO,
+            centerX - cos(angle) * radius * MAIN_SHORT_PART_SECOND_HAND_TO_RADIUS_RATIO,
+            centerY - sin(angle) * radius * MAIN_SHORT_PART_SECOND_HAND_TO_RADIUS_RATIO,
+            additionalPartSecondHandPaint
+        )
     }
 
     override fun onDraw(canvas: Canvas) {
         drawBase(canvas = canvas)
         drawFrame(canvas = canvas)
+        drawSecondHand(canvas = canvas, second = 1)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
