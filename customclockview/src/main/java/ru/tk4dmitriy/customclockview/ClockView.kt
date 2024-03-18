3ca1a498b609600f880538cdd3b7fb99d71e2f06
Dmitriy Tkachenko <tk4dmitriy@gmail.com>
2024-03-18 23:27:38 +0300

    Added drawing of the minute hand based on the current second. Trigonometry is used to draw an second hand.

--- customclockview/src/main/java/ru/tk4dmitriy/customclockview/ClockView.kt
+++ customclockview/src/main/java/ru/tk4dmitriy/customclockview/ClockView.kt
@@ -2,6 +2,7 @@ package ru.tk4dmitriy.customclockview
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
@@ -13,11 +14,16 @@ import kotlin.math.sin
 private const val FRAME_TO_RADIUS_RATIO = 2.0f / 21.0f
 private const val MAIN_LONG_PART_SECOND_HAND_TO_RADIUS_RATIO = 32.0f / 45.0f
 private const val MAIN_SHORT_PART_SECOND_HAND_TO_RADIUS_RATIO = 2.0f / 35.0f
-private const val ADDITIONAL_PART_SECOND_HAND_TO_RADIUS_RATIO = 9.0f / 35.0f
+private const val ADDITIONAL_PART_SECOND_HAND_TO_RADIUS_RATIO = 5.0f / 21.0f
 private const val WIDTH_MAIN_PART_SECOND_HAND_TO_RADIUS_RATIO = 1.0f / 63.0f
 private const val WIDTH_ADDITIONAL_PART_SECOND_HAND_TO_RADIUS_RATIO = 1.0f / 35.0f
 private const val SECOND_TO_ANGLE_RATIO = Math.PI / 30
+private const val SECOND_TO_MINUTE_ANGLE_RATIO = Math.PI / 1800
 private const val OFFSET_ANGLE = Math.PI / 2
+
+private const val LONG_PART_MINUTE_HANG_TO_RADIUS_RATIO = 29.0f / 45.0f
+private const val SHORT_PART_MINUTE_HANG_TO_RADIUS_RATIO = 5.0f / 21.0f
+private const val WIDTH_MINUTE_SECOND_TO_RADIUS_RATIO = 2.0f / 45.0f
 
 class ClockView @JvmOverloads constructor(
     context: Context,
@@ -27,6 +33,7 @@ class ClockView @JvmOverloads constructor(
     private val framePaint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val mainPartSecondHandPaint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val additionalPartSecondHandPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val minuteHandPaint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     private val defWidth: Int = resources.getDimension(R.dimen.defWidth).toInt()
     private val defHeight: Int = resources.getDimension(R.dimen.defHeight).toInt()
@@ -40,6 +47,7 @@ class ClockView @JvmOverloads constructor(
     var baseColor = 0
     var frameColor = 0
     var secondHandColor = 0
+    var minuteHandColor = 0
 
     init {
         context.withStyledAttributes(attrs, R.styleable.ClockView, R.attr.clockViewStyle) {
@@ -52,6 +60,10 @@ class ClockView @JvmOverloads constructor(
                 ContextCompat.getColor(context, R.color.black)
             )
             secondHandColor = getColor(
+                R.styleable.ClockView_frameColor,
+                ContextCompat.getColor(context, R.color.black)
+            )
+            minuteHandColor = getColor(
                 R.styleable.ClockView_frameColor,
                 ContextCompat.getColor(context, R.color.black)
             )
@@ -69,6 +81,9 @@ class ClockView @JvmOverloads constructor(
         additionalPartSecondHandPaint.apply {
             color = secondHandColor
         }
+        minuteHandPaint.apply {
+            color = minuteHandColor
+        }
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -83,6 +98,8 @@ class ClockView @JvmOverloads constructor(
             radius * WIDTH_MAIN_PART_SECOND_HAND_TO_RADIUS_RATIO
         additionalPartSecondHandPaint.strokeWidth =
             radius * WIDTH_ADDITIONAL_PART_SECOND_HAND_TO_RADIUS_RATIO
+        minuteHandPaint.strokeWidth =
+            radius * WIDTH_MINUTE_SECOND_TO_RADIUS_RATIO
     }
 
     private fun drawBase(canvas: Canvas) {
@@ -110,10 +127,22 @@ class ClockView @JvmOverloads constructor(
         )
     }
 
+    private fun drawMinuteHand(canvas: Canvas, second: Int) {
+        angle = (second * SECOND_TO_MINUTE_ANGLE_RATIO - OFFSET_ANGLE).toFloat()
+        canvas.drawLine(
+            centerX - cos(angle) * radius * SHORT_PART_MINUTE_HANG_TO_RADIUS_RATIO,
+            centerY - sin(angle) * radius * SHORT_PART_MINUTE_HANG_TO_RADIUS_RATIO,
+            centerX + cos(angle) * radius * LONG_PART_MINUTE_HANG_TO_RADIUS_RATIO,
+            centerY + sin(angle) * radius * LONG_PART_MINUTE_HANG_TO_RADIUS_RATIO,
+            minuteHandPaint
+        )
+    }
+
     override fun onDraw(canvas: Canvas) {
         drawBase(canvas = canvas)
         drawFrame(canvas = canvas)
-        drawSecondHand(canvas = canvas, second = 1)
+        drawSecondHand(canvas = canvas, second = 15)
+        drawMinuteHand(canvas = canvas, second = 15)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
